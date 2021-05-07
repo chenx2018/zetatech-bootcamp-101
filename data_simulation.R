574e8252f3e922979f89f39bae8a42478ddf0dc0
chenx2018 <chenx529@gmail.com>
2021-05-07 18:07:54 +0800

    update

--- data_simulation.R
+++ data_simulation.R
@@ -61,9 +61,86 @@ dat <- dat %>%
   mutate(cellphone = generate_cellphone(size = n_distinct(dat$cardNum))) %>%
   unnest(data)
 
-dat %>%
-  summary()
+# generate fake homephone-----------------------------------
+dat <- dat %>%
+  group_nest(cardNum) %>%
+  mutate(
+    homephone = paste0(
+      "(0755)-",
+      sample(10000121:99990100, size = n_distinct(dat$cardNum))
+    )
+  ) %>%
+  unnest(data)
 
+# generate fake cardHolder Name-----------------------------------
+
+# name 数据集
+fakeName <- read_rds("simulation_resource/fakeNames.rds")
+
+fake_name_gender_dat <- fakeName %>%
+  mutate(wt = case_when(
+    gender == "女" ~ 90,
+    TRUE ~ 10
+  )) %>%
+  slice_sample(n = n_distinct(dat$cardNum), weight_by = wt)
+
+fake_name_gender_dat %>%
+  count(gender)
+
+dat <- dat %>%
+  group_nest(cardNum) %>%
+  bind_cols(select(fake_name_gender_dat, cardHolder = name, gender))%>%
+  unnest(data)
+
+# generate fake dob-----------------------------------
+
+generate_age <- function(size) {
+  x <- c(
+    rnorm(n = round(20000*.2), 25, 2),
+    rnorm(n = round(20000*.3), 35, 3),
+    rnorm(n = round(20000*.35), 45, 3),
+    rnorm(n = round(20000*.2), 50, 4),
+    rnorm(n = round(20000*.05), 55, 2)
+  )
+  x <- x[x>20 & x<60]
+  sample(round(x), size = size, replace = TRUE)
+}
+
+
+# add age
+dat <- dat %>%
+  group_nest(cardNum) %>%
+  mutate(age = generate_age(nrow(.))) %>%
+  unnest(data)
+
+# add dob
+dat <- dat %>%
+  arrange(date, prodNum) %>%
+  mutate(dob = as.Date(Sys.Date() - age*365))
+
+# generate wechat staff-----------------------------------
+generate_wechat_staff <- function(size) {
+  name <- factor(sample(fake_name_gender_dat$name, size = 40))
+  id <- fct_anon(name, prefix = "00")
+  info <- sample(
+    c("本人微信", "企业微信"),
+    size = 40, replace = TRUE, prob = c(.3, .7)
+  )
+  res <- glue::glue("{name} \u27a3 工号:{id} \u27a3 {info}")
+  sample(res, size = size, replace = TRUE) %>%
+    as.character()
+}
+
+# generate fake wechat staff data-----------------------------------
+fake_wechat_staff_dat <- dat %>%
+  distinct(cellphone) %>%
+  mutate(wechat_staff = generate_wechat_staff(nrow(.)))
 
 
 # save data-----------------------------------
+dat %>%
+  write_rds("data/fake_fulldata.rds", compress = "gz")
+
+fake_wechat_staff_dat %>%
+  write_rds("data/fake_wechat_staff_dat.rds", compress = "gz")
+
